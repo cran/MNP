ab74f149017486bc2c3e62c30a3ad6ace8e35048
Kosuke Imai <kimai@princeton.edu>
2006-09-21 00:00:00 +0000

    version 2.3-9

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,4 +1,4 @@
-".onLoad" <- function(lib, pkg) {
+".onAttach" <- function(lib, pkg) {
   mylib <- dirname(system.file(package = pkg))
   title <- packageDescription(pkg, lib = mylib)$Title
   ver <- packageDescription(pkg, lib = mylib)$Version
