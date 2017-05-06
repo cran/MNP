779cbde9190f28f551cb1ef8e043af5a3e255b1d
Kosuke Imai <kimai@princeton.edu>
2017-05-06 04:17:20 +0000

    version 3.0-1

--- R/print.summary.mnp.R
+++ R/print.summary.mnp.R
@@ -1,3 +1,16 @@
+#' Print the summary of the results for the Multinomial Probit Models
+#' 
+#' \code{summary} print method for class \code{mnp}.
+#' 
+#' @aliases print.summary.mnp
+#' @param x An object of class \code{summary.mnp}.
+#' @param digits the number of significant digits to use when printing.
+#' @param ... further arguments passed to or from other methods.
+#' @author Kosuke Imai, Department of Politics, Princeton University
+#' \email{kimai@Princeton.Edu}
+#' @seealso \code{mnp}
+#' @keywords methods
+#' @export print.summary.mnp
 print.summary.mnp <- function(x, digits = max(3, getOption("digits") - 3), ...) {
 
   cat("\nCall:\n")
